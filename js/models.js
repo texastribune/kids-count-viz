016674bb4aec621770eb5db79ebab100588a340d
danhillreports <danhillreports@gmail.com>
2013-12-02 16:36:26 -0600

    text next to button

--- js/models.js
+++ js/models.js
@@ -1,13 +1,9 @@
 var CountyModel = Backbone.Model.extend({
-    initialize: function() {
-        this.calculatePovertyChange();
-    },
-
     calculatePovertyChange: function() {
         var old = this.get("pct_poverty_child")[0];
         var new_data = this.get("pct_poverty_child")[11];
         var diff = Math.round(((new_data - old)/old) * 1000) / 10;
 
-        this.set("poverty_change", diff);
+        return diff;
     }
 });
