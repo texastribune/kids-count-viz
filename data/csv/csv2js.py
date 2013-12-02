d612bbd5125f7b0440554201693e96d78de07469
danhillreports <danhillreports@gmail.com>
2013-12-02 17:41:23 -0600

    committing atrocities for 2000 and 2011 data points

--- data/csv/csv2js.py
+++ data/csv/csv2js.py
@@ -29,15 +29,38 @@ for row in reader:
                     d["pct_poverty_child"].append(float(row[4]))
                     d["pct_unemployment"].append(float(row[5]))
 
-pop = open("total-pop.csv")
+
+def importData(reader, field_name, pos):
+    for row in reader:
+        for d in child:
+            if d["county"] == row[0]:
+                key = str(row[2] + '-' + str(row[1]))
+                try:
+                    d[field_name][key] = int(row[pos])
+                except KeyError:
+                    d[field_name] = {}
+                    d[field_name][key] = int(row[pos])
+                except ValueError:
+                    d[field_name][key] = float(row[pos])
+
+pop = open("pop.csv")
 pop_reader = csv.reader(pop)
 pop_reader.next()
+importData(pop_reader, "pop", 3)
 
-for row in pop_reader:
-    for d in child:
-        if d["county"] == row[1]:
-            d["poverty_raw_children"] = int(row[4])
-            d["unemployment_raw"] = int(row[5])
-            d["child_pop"] = int(row[6])
-            d["pop"] = int(row[7])
+child_pop = open("child-population.csv")
+child_pop_reader = csv.reader(child_pop)
+child_pop_reader.next()
+importData(child_pop_reader, "child_pop", 3)
+
+poverty = open("poverty-2-years.csv")
+poverty_reader = csv.reader(poverty)
+poverty_reader.next()
+importData(poverty_reader, "poverty_raw_children", 3)
+
+unemp = open("unemp.csv")
+unemp_reader = csv.reader(unemp)
+unemp_reader.next()
+importData(unemp_reader, "unemployment_raw", 3)
+
 print child
