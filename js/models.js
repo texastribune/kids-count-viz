dfaff1eec0e8bda989cace3ab8d118915ce93dca
danhillreports <danhillreports@gmail.com>
2013-12-02 18:18:08 -0600

    add child population change to text

--- js/models.js
+++ js/models.js
@@ -1,19 +1,33 @@
 var CountyModel = Backbone.Model.extend({
     calculatePovertyChange: function() {
-        var old = this.get("poverty_raw_children")["Number-2011"];
-        var new_data = this.get("poverty_raw_children")["Number-2000"];
+        var old = this.get("poverty_raw_children")["Number-2000"];
+        var new_data = this.get("poverty_raw_children")["Number-2011"];
         var diff = Math.round(((new_data - old)/old) * 1000) / 10;
         var diff_text = '';
 
-        diff > 0 ? diff_text = 'increased ' + diff : diff_text = 'decreased ' + diff;
+        diff > 0 ? diff_text = 'increased ' + diff : diff_text = 'decreased ' + Math.abs(diff);
+        return diff_text;
+    },
+
+    calculateChildPopChange: function() {
+        var old = this.get("child_pop")["Number-2000"];
+        var new_data = this.get("child_pop")["Number-2011"];
+
+
+        var diff = Math.round(((new_data - old) / old) * 1000) / 10;
+        var diff_text = '';
+
+        diff > 0 ? diff_text = 'increased ' + diff : diff_text = 'decreased ' + Math.abs(diff);
         return diff_text;
     },
 
     calculateDiffFromTexas: function() {
-        var diff = Math.round((this.get("poverty_raw_children")["Percent-2011"] - texas.get("poverty_raw_children")["Percent-2011"]) * 1000) / 10;
+        var county = this.get("poverty_raw_children")["Percent-2011"];
+        var tx = texas.get("poverty_raw_children")["Percent-2011"]
+        var diff = Math.round((county - tx) * 1000) / 10;
         var diff_text = '';
 
-        diff > 0 ? diff_text = diff + ' percentage points higher than ' : diff_text = diff + ' percentage points lower than ';
+        diff > 0 ? diff_text = diff + ' percentage points higher than ' : diff_text = Math.abs(diff) + ' percentage points lower than ';
         return diff_text;
     },
 
